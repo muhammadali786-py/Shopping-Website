9e73d9298111e83c5693d5d7ab2c04259586cee8
muhammadali786-py <muhammadaliamaan515@gmail.com>
2025-03-15 13:42:10 +0500

    Online sinple store project

---
+++ WebApp/interface.py
@@ -0,0 +1,18 @@
+import operator
+from django.http import HttpResponse
+from django.shortcuts import render
+from . import counter_app
+
+def home(request):
+    return render(request, 'home.html',)
+def portfolio(request):
+    return render(request, 'portfolio.html')
+def result(request):
+    article = request.GET['article']
+    words_count,sorted_dict = counter_app.count(article)
+    return render(request, 'result.html', {'article': article , 'words_count' : words_count, 'words_dict' : sorted_dict})
+def store(request):
+    return render(request, 'store.html',)
+def contact(request):
+    return render(request, 'contact.html',)
+
